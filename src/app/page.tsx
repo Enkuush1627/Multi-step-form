97674d87d3397b470c868b764f66009029de602d
Enkvvsh <enkuush@Enkuushs-MacBook-Air.local>
2025-12-11 07:26:36 +0800

    form.4

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,21 @@
 "use client";
 
-import Page from "./form/page";
+import { useState } from "react";
+import { FormPage1 } from "./_components/step1";
+import { FormPage2 } from "./_components/step2";
+import { FormPage3 } from "./_components/step3";
+import { FormPage4 } from "./_components/success";
 
 export default function Home() {
+  const [step, setStep] = useState(1);
   return (
     <div className="bg-gray-300 w-screen h-screen flex justify-center items-center">
-      <Page />
+      <>
+        {step === 1 && <FormPage1 />}
+        {step === 2 && <FormPage2 />}
+        {step === 3 && <FormPage3 />}
+        {step === 4 && <FormPage4 />}
+      </>
     </div>
   );
 }
